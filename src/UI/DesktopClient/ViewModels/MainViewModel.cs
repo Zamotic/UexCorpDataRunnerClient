59b4fec75321f69964f93b70e06192d2c8e19db7
Brian Tomazic <batomazic@gmail.com>
2022-04-11 00:01:03 -0400

    Added BindableCommodityPrice list to MainViewModel

--- src/UI/DesktopClient/ViewModels/MainViewModel.cs
+++ src/UI/DesktopClient/ViewModels/MainViewModel.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using UexCorpDataRunner.DesktopClient.Core;
+using UexCorpDataRunner.DesktopClient.Model;
 using UexCorpDataRunner.DesktopClient.Notifications;
 
 namespace UexCorpDataRunner.DesktopClient.ViewModels;
@@ -16,12 +16,40 @@ public class MainViewModel : ViewModelBase
 {
     public readonly IMessenger _Messenger;
 
+    private IList<BindableCommodityPrice>? _BindableCommodityPrices;
+    public IList<BindableCommodityPrice>? BindableCommodityPrices
+    {
+        get => _BindableCommodityPrices;
+        set => SetProperty(ref _BindableCommodityPrices, value);
+    }
+
     public MainViewModel(IMessenger messenger)
     {
         IsEnabled = true;
         _Messenger = messenger;
         _Messenger.Register<ShowUserInterfaceNotification>(this, ShowUserInterfaceNotified);
         _Messenger.Register<CloseSettingsInterfaceNotification>(this, CloseSettingsInterfaceNotified);
+        BindableCommodityPrices = new List<BindableCommodityPrice>()
+        {
+            new BindableCommodityPrice(new CommodityPrice()
+            {
+                Name = "Aluminum",
+                OldPrice = 1.11m,
+                MinPrice = 1.10m,
+                MaxPrice = 1.20m,
+                BestPrice = 1.10m,
+                BestLocation = "HDMS-Perlman, Magda"
+            }),
+            new BindableCommodityPrice(new CommodityPrice()
+            {
+                Name = "Diamond",
+                OldPrice = 5.85m,
+                MinPrice = 5.85m,
+                MaxPrice = 6.27m,
+                BestPrice = 5.85m,
+                BestLocation = "HDMS-Hahn, Magda"
+            })
+        };
     }
 
     public ICommand HideUserInterfaceCommand => new RelayCommand(HideUserInterfaceCommandExecute);
