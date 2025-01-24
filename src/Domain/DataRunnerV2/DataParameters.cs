6bf6e6c7d1d6b00d85c39591b7d36a3ebef4f70e
Brian Tomazic <batomazic@gmail.com>
2025-01-24 00:27:02 -0500

    Added DataParameters domain model, Mapper, and UnitTests

---
+++ src/Domain/DataRunnerV2/DataParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UexCorpDataRunner.Domain.DataRunnerV2;
+public class DataParameters
+{
+    public DataParametersGlobal Global { get; set; } = new();
+
+    public class DataParametersGlobal
+    {
+        public bool IsAcceptingReports { get; set; }
+        public string? GameVersion { get; set; }
+        public string? GameVersionPtu { get; set; }
+        public int EvaluationPeriodDays { get; set; }
+    }
+}
